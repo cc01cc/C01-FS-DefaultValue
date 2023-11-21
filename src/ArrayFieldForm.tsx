1d547974f5c6dba9ad3573c1b785c9cfb984c0b8
cc01cc <cc01cc@foxmail.com>
2023-11-21 15:15:37 +0800

    feat: 添加刷新数据时的加载状态

--- src/ArrayFieldForm.tsx
+++ src/ArrayFieldForm.tsx
@@ -15,7 +15,7 @@
  */
 
 import React, {useEffect, useRef, useState} from 'react';
-import {ArrayField, Button, Form, Toast} from '@douyinfe/semi-ui';
+import {ArrayField, Button, Form, Spin, Toast} from '@douyinfe/semi-ui';
 import {IconMinusCircle, IconPlusCircle} from '@douyinfe/semi-icons';
 import useTableFieldState from "./hooks/useTableFieldState";
 import {bitable} from "@lark-base-open/js-sdk";
@@ -31,6 +31,8 @@ function ArrayFieldForm() {
     } = useTableFieldState();
     const [data, setData] = useState<{ name: string, defaultValue: string; }[]>();
     const formApi = useRef<any>();
+    const [loading, setLoading] = useState(false)
+    const [loadingContent, setLoadingContent] = useState('')
 
     const [fieldListCanChoose, setFieldListCanChoose] = useState<{ name: string, id: string; }[]>([]);
     // const [optionListCanChoose, setOptionListCanChoose] = useState<any>([]);
@@ -63,6 +65,8 @@ function ArrayFieldForm() {
      * 4. 初始化可选字段列表
      */
     const fetchNewInfo = async () => {
+        setLoading(true)
+        setLoadingContent('刷新数据中')
         // todo 刷新时添加加载状态
         const selection = await bitable.base.getSelection();
         console.log("selection", selection);
@@ -100,6 +104,7 @@ function ArrayFieldForm() {
 
         // 初始化可选字段列表
         setFieldListCanChoose(fieldMetaList.map(({name, id}) => ({name, id})))
+        setLoading(false)
         console.log('fieldListCanChoose', fieldListCanChoose)
     }
 
@@ -147,133 +152,137 @@ function ArrayFieldForm() {
     }
 
     return (
-        <Form
-            wrapperCol={{span: 20}}
-            labelCol={{span: 100}}
-            style={{width: 500}}
-            labelPosition='top'
-            // labelWidth='100px'
-            allowEmpty
-            getFormApi={(a: any) => formApi.current = a}
-            onChange={(formState: any) => formApi.current.formState = formState}
-        >
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <Form.Select style={{width: 200}} onChange={onSelectTable} label='Table' field="table">
-                    {
-                        Array.isArray(tableInfo?.tableMetaList) && tableInfo?.tableMetaList.map(({id, name}) =>
-                            <Form.Select.Option key={id} value={id}>{name}</Form.Select.Option>)
-                    }
-                </Form.Select>
-                <Button
-                    theme="solid"
-                    type="primary"
-                    className="bt1"
-                    onClick={useLastRecord}
-                    style={{margin: 12, alignSelf: 'flex-end'}}
-                >
-                    {"复用上一次记录"}
-                </Button>
-                <Button
-                    theme="solid"
-                    type="primary"
-                    className="bt1"
-                    onClick={fetchNewInfo}
-                    style={{margin: 12, alignSelf: 'flex-end'}}
-                >
-                    {"刷新数据"}
-                </Button>
-            </div>
-
-
-            <ArrayField field='field' initValue={data}>
-                {({add, arrayFields, addWithInitValue}) => (
-                    <React.Fragment>
-                        <Button onClick={add} icon={<IconPlusCircle/>} theme='light'>添加字段</Button>
+        <Spin style={{height: '100vh'}} tip={loadingContent} size="large" spinning={loading}>
+            <Form
+                wrapperCol={{span: 20}}
+                labelCol={{span: 100}}
+                style={{width: 500}}
+                labelPosition='top'
+                // labelWidth='100px'
+                allowEmpty
+                getFormApi={(a: any) => formApi.current = a}
+                onChange={(formState: any) => formApi.current.formState = formState}
+            >
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <Form.Select style={{width: 200}} onChange={onSelectTable} label='Table' field="table">
                         {
-                            arrayFields.map(({field, key, remove}, i) => (
-                                <div key={key} style={{width: '100%', display: 'flex'}}>
-                                    <Form.Select
-                                        field={`${field}[name]`}
-                                        label={`字段名`}
-                                        style={{width: 120, marginRight: 20}}
-                                        onChange={onSelectField}
-                                    >
-                                        {
-                                            fieldListCanChoose.map(({id, name}) => <Form.Select.Option key={id}
-                                                                                                       value={id}>{name}</Form.Select.Option>)
-                                        }
-                                    </Form.Select>
-                                    <Form.Select
-                                        field={`${field}[defaultValue]`}
-                                        label={`默认值`}
-                                        style={{width: 120}}
-                                        optionList={[
-                                            {label: 'Engineer', value: 'Engineer'},
-                                            {label: 'Designer', value: 'Designer'},
-                                        ]}
-                                    >
-
-                                    </Form.Select>
-                                    <Button
-                                        theme="solid"
-                                        type="primary"
-                                        className="bt1"
-                                        // onClick={clickFill}
-                                        style={{margin: 12, alignSelf: 'flex-end'}}
-                                    >
-                                        {"填充"}
-                                    </Button>
-
-                                    <Form.Switch
-                                        field={`${field}[autoInput]`}
-                                        label={{text: '自动填充', width: '100%'}}
-                                        // noLabel={true}
-                                        checkedText='开'
-                                        uncheckedText='关'
-                                        // onChange={(v) => openAutoInput(v)}
-                                    />
-
-                                    <Button
-                                        type='danger'
-                                        theme='borderless'
-                                        icon={<IconMinusCircle/>}
-                                        onClick={remove}
-                                        style={{margin: 12, alignSelf: 'flex-end'}}
-                                    />
-                                </div>
-                            ))
+                            Array.isArray(tableInfo?.tableMetaList) && tableInfo?.tableMetaList.map(({id, name}) =>
+                                <Form.Select.Option key={id} value={id}>{name}</Form.Select.Option>)
                         }
-                    </React.Fragment>
-                )}
-            </ArrayField>
+                    </Form.Select>
+                    <Button
+                        theme="solid"
+                        type="primary"
+                        className="bt1"
+                        onClick={useLastRecord}
+                        style={{margin: 12, alignSelf: 'flex-end'}}
+                    >
+                        {"复用上一次记录"}
+                    </Button>
+                    <Button
+                        theme="solid"
+                        type="primary"
+                        className="bt1"
+                        onClick={fetchNewInfo}
+                        style={{margin: 12, alignSelf: 'flex-end'}}
+                    >
+                        {"刷新数据"}
+                    </Button>
 
-            <div style={{display: 'flex', marginTop: 20}}>
+                </div>
 
-                <Form.Switch
-                    field="autoInput"
-                    labelPosition={"left"}
-                    labelWidth={100}
-                    label={'全部自动填充'}
-                    // noLabel={true}
-                    checkedText='开'
-                    uncheckedText='关'
-                    // onChange={(v) => openAutoInput(v)}
-                />
-                <Button
-                    theme="solid"
-                    type="primary"
-                    className="bt1"
-                    // onClick={clickFill}
-                    style={{margin: 12, alignSelf: 'flex-end'}}
-                >
-                    {"全部填充"}
-                </Button>
-                <Button
-                    htmlType="reset"
-                    style={{margin: 12, alignSelf: 'flex-end'}}
-                >重置</Button>
-            </div>
-        </Form>
+
+                <ArrayField field='field' initValue={data}>
+                    {({add, arrayFields, addWithInitValue}) => (
+                        <React.Fragment>
+                            <Button onClick={add} icon={<IconPlusCircle/>} theme='light'>添加字段</Button>
+                            {
+                                arrayFields.map(({field, key, remove}, i) => (
+                                    <div key={key} style={{width: '100%', display: 'flex'}}>
+                                        <Form.Select
+                                            field={`${field}[name]`}
+                                            label={`字段名`}
+                                            style={{width: 120, marginRight: 20}}
+                                            onChange={onSelectField}
+                                        >
+                                            {
+                                                fieldListCanChoose.map(({id, name}) => <Form.Select.Option key={id}
+                                                                                                           value={id}>{name}</Form.Select.Option>)
+                                            }
+                                        </Form.Select>
+                                        <Form.Select
+                                            field={`${field}[defaultValue]`}
+                                            label={`默认值`}
+                                            style={{width: 120}}
+                                            optionList={[
+                                                {label: 'Engineer', value: 'Engineer'},
+                                                {label: 'Designer', value: 'Designer'},
+                                            ]}
+                                        >
+
+                                        </Form.Select>
+                                        <Button
+                                            theme="solid"
+                                            type="primary"
+                                            className="bt1"
+                                            // onClick={clickFill}
+                                            style={{margin: 12, alignSelf: 'flex-end'}}
+                                        >
+                                            {"填充"}
+                                        </Button>
+
+                                        <Form.Switch
+                                            field={`${field}[autoInput]`}
+                                            label={{text: '自动填充', width: '100%'}}
+                                            // noLabel={true}
+                                            checkedText='开'
+                                            uncheckedText='关'
+                                            // onChange={(v) => openAutoInput(v)}
+                                        />
+
+                                        <Button
+                                            type='danger'
+                                            theme='borderless'
+                                            icon={<IconMinusCircle/>}
+                                            onClick={remove}
+                                            style={{margin: 12, alignSelf: 'flex-end'}}
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </React.Fragment>
+                    )}
+                </ArrayField>
+
+                <div style={{display: 'flex', marginTop: 20}}>
+
+                    <Form.Switch
+                        field="autoInput"
+                        labelPosition={"left"}
+                        labelWidth={100}
+                        label={'全部自动填充'}
+                        // noLabel={true}
+                        checkedText='开'
+                        uncheckedText='关'
+                        // onChange={(v) => openAutoInput(v)}
+                    />
+                    <Button
+                        theme="solid"
+                        type="primary"
+                        className="bt1"
+                        // onClick={clickFill}
+                        style={{margin: 12, alignSelf: 'flex-end'}}
+                    >
+                        {"全部填充"}
+                    </Button>
+                    <Button
+                        htmlType="reset"
+                        style={{margin: 12, alignSelf: 'flex-end'}}
+                    >重置</Button>
+                </div>
+
+            </Form>
+        </Spin>
     );
 }
 
