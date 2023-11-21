a08a63b586ca179015d74f0daf8ce45894b65758
cc01cc <cc01cc@foxmail.com>
2023-11-21 14:31:49 +0800

    refactor: 拆分部分 hook

--- src/ArrayFieldForm.tsx
+++ src/ArrayFieldForm.tsx
@@ -1,41 +1,42 @@
+/*
+ *    Copyright 2023. cc01cc
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 import React, {useEffect, useRef, useState} from 'react';
 import {ArrayField, Button, Form} from '@douyinfe/semi-ui';
 import {IconMinusCircle, IconPlusCircle} from '@douyinfe/semi-icons';
-import {IField, IFieldMeta as FieldMeta, ISelectFieldOption, ITable, ITableMeta} from "@lark-base-open/js-sdk";
+import useTableFieldState from "./hooks/useTableFieldState";
 
 function ArrayFieldForm() {
-    const [tableInfo, setTableInfo] = useState<{
-        /** 当前所选的table,默认为打开插件时的table */
-        table: ITable,
-        /** 当前所选table的元信息 */
-        tableMeta: ITableMeta
-        /** 所有的table元信息 */
-        tableMetaList: ITableMeta[],
-        /** 所有table的实例 */
-        tableList: ITable[]
-    }>();
-    const [fieldInfo, setFieldInfo] = useState<{
-        /**当前所选field的实例 */
-        field: IField | undefined
-        /** 当前所选field的元信息 */
-        fieldMeta: FieldMeta | undefined
-        /** tableInfo.table的所有field实例 */
-        fieldList: IField[],
-        /** tableInfo.table的所有field元信息 */
-        fieldMetaList: FieldMeta[]
-    }>()
-    const [options, setOptions] = useState<ISelectFieldOption[]>();
-
+    const {
+        tableInfo,
+        fieldInfo,
+        setTableInfo,
+        setFieldInfo,
+        options,
+        setOptions
+    } = useTableFieldState();
     const [data, setData] = useState<{ name: string, defaultValue: string; }[]>();
     const formApi = useRef<any>();
+
 
     useEffect(() => {
         setData([
             {name: 'Engineer', defaultValue: 'Engineer'},
             {name: 'Designer', defaultValue: 'Designer'},
         ])
-
-        // console.log(formApi.getValues)
     }, []);
     // 复用上一次的记录
     const useLastRecord = () => {
